b982425a4d54368d2b53b1ec68f3d91ff4fbc0b7
Kapa Kudaibergenov <kudkapa59@gmail.com>
2024-11-21 15:44:21 +0100

    Update fetch-secrets.js

--- fetch-secrets.js
+++ fetch-secrets.js
@@ -1,5 +1,6 @@
-const { DefaultAzureCredential } = require("@azure/identity");
-const { SecretClient } = require("@azure/keyvault-secrets");
+const {React} = require("@react");
+#const { DefaultAzureCredential } = require("@azure/identity");
+#const { SecretClient } = require("@azure/keyvault-secrets");
 
 async function fetchSecrets() {
   const credential = new DefaultAzureCredential();
