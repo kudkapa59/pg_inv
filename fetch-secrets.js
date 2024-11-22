d59477abcf06991a076369d56ca8dfd71299368b
Kapa Kudaibergenov <kudkapa59@gmail.com>
2024-11-22 16:08:40 +0100

    Update fetch-secrets.js

--- fetch-secrets.js
+++ fetch-secrets.js
@@ -1,29 +1,27 @@
-import { DefaultAzureCredential } from "@azure/identity";
-//const { DefaultAzureCredential } = require("@azure/identity");
-// const { SecretClient } = require("@azure/keyvault-secrets");
-import { SecretClient } from "@azure/keyvault-secrets";
+//import { DefaultAzureCredential } from "@azure/identity";
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+//import { SecretClient } from "@azure/keyvault-secrets";
 
-async function fetchSecrets() {
-  const credential = new DefaultAzureCredential();
-  //const vaultName = process.env.AZURE_KEYVAULT_NAME;
-  //console.log(credential)
-  const url = process.env.AZURE_KEY_VAULT_URL;
-  console.log(`url = ${url}`)
-  console.log("Check")
-  const client = new SecretClient(url, credential);
+//async function fetchSecrets() {
+const credential = new DefaultAzureCredential();
+const url = process.env.AZURE_KEY_VAULT_URL;
+console.log(`url = ${url}`)
+console.log("Check")
+const client = new SecretClient(url, credential);
 
-  try {
-    // console.log(`${}`)
-    for await (const secretProperties of client.listPropertiesOfSecrets()) {
-      const secretName = secretProperties.name;
-      context.log(`Secretname ${secretName}`)
-      // const secret = await client.getSecret(secretName);
-      // console.log(`${secretName}=${secret.value}`);
-    }
-  } catch (error) {
-    console.error("Error fetching secrets:", error);
-    process.exit(1);
-  }
-}
+  // try {
+  //   // console.log(`${}`)
+  //   for await (const secretProperties of client.listPropertiesOfSecrets()) {
+  //     const secretName = secretProperties.name;
+  //     context.log(`Secretname ${secretName}`)
+  //     // const secret = await client.getSecret(secretName);
+  //     // console.log(`${secretName}=${secret.value}`);
+  //   }
+  // } catch (error) {
+  //   console.error("Error fetching secrets:", error);
+  //   process.exit(1);
+  // }
+//}
 
-fetchSecrets();
+//fetchSecrets();
