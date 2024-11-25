62e224bf9ddcfd58563c90614ab38efd7282f4c8
Kapa Kudaibergenov <kudkapa59@gmail.com>
2024-11-25 14:44:16 +0100

    Update fetch-secrets.js

--- fetch-secrets.js
+++ fetch-secrets.js
@@ -2,32 +2,59 @@
 const {
   ClientSecretCredential,
   ChainedTokenCredential,
-  EnvironmentCredential
+  EnvironmentCredential,
+  ManagedIdentityCredential
 } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 //import 'dotenv/config'
 //import { SecretClient } from "@azure/keyvault-secrets";
 
+
+function withSystemAssignedManagedIdentityCredential() {
+  const credential = new ManagedIdentityCredential();
+  const url = process.env.AZURE_KEY_VAULT_URL;
+  const client = new SecretClient(url, credential);
+}
+
+
 async function fetchSecrets() {
   // const credential = new DefaultAzureCredential();
-  const credential = new EnvironmentCredential();
+
+  const credential = new ManagedIdentityCredential();
   const url = process.env.AZURE_KEY_VAULT_URL;
-  console.log(`url = ${url}`)
-  console.log("Check")
   const client = new SecretClient(url, credential);
 
-  try {
-    // console.log(`${}`)
-    for await (const secretProperties of client.listPropertiesOfSecrets()) {
-      // const secretName = secretProperties.name;
-      // const secret = await client.getSecret(secretName);
-      // console.log(`${secretName}=${secret.value}`);
+  
+  // const credential = new EnvironmentCredential();
+  // const url = process.env.AZURE_KEY_VAULT_URL;
+  // console.log(`url = ${url}`)
+  // console.log("Check")
+  // const client = new SecretClient(url, credential);
+  // List the secrets we have, by page
+  console.log("Listing secrets by page");
+  for await (const page of client.listPropertiesOfSecrets().byPage({ maxPageSize: 2 })) {
+    for (const secretProperties of page) {
+      if (secretProperties.enabled) {
+        const secret = await client.getSecret(secretProperties.name);
+        console.log("secret: ", secret);
+      }
     }
-  } catch (error) {
-    console.error("Error fetching secrets:", error);
-    process.exit(1);
+    console.log("--page--");
+  }
+
+    // List the secrets we have, all at once
+  console.log("Listing secrets all at once");
+  for await (const secretProperties of client.listPropertiesOfSecrets()) {
+    if (secretProperties.enabled) {
+      const secret = await client.getSecret(secretProperties.name);
+      console.log("secret: ", secret);
+    }
   }
   
 }
 
-fetchSecrets();
+fetchSecrets().catch((error) => {
+  console.error("An error occurred:", error);
+  process.exit(1);
+});
+
