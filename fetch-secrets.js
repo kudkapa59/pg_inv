19c3961d1f5d32b201ac34f2e006eafae031ad5f
Kapa Kudaibergenov <kudkapa59@gmail.com>
2024-11-25 16:09:50 +0100

    Update fetch-secrets.js

--- fetch-secrets.js
+++ fetch-secrets.js
@@ -25,32 +25,30 @@ async function fetchSecrets() {
   const url = process.env.AZURE_KEY_VAULT_URL;
   const client = new SecretClient(url, credential);
 
-  
-  // const credential = new EnvironmentCredential();
-  // const url = process.env.AZURE_KEY_VAULT_URL;
-  // console.log(`url = ${url}`)
-  // console.log("Check")
-  // const client = new SecretClient(url, credential);
-  // List the secrets we have, by page
-  console.log("Listing secrets by page");
-  for await (const page of client.listPropertiesOfSecrets().byPage({ maxPageSize: 2 })) {
-    for (const secretProperties of page) {
-      if (secretProperties.enabled) {
-        const secret = await client.getSecret(secretProperties.name);
-        console.log("secret: ", secret);
-      }
-    }
-    console.log("--page--");
-  }
+  // Get secret
+  const secret = await client.getSecret("client-id");
+  console.log(secret)
 
-    // List the secrets we have, all at once
-  console.log("Listing secrets all at once");
-  for await (const secretProperties of client.listPropertiesOfSecrets()) {
-    if (secretProperties.enabled) {
-      const secret = await client.getSecret(secretProperties.name);
-      console.log("secret: ", secret);
-    }
-  }
+  // // List the secrets we have, by page
+  // console.log("Listing secrets by page");
+  // for await (const page of client.listPropertiesOfSecrets().byPage({ maxPageSize: 2 })) {
+  //   for (const secretProperties of page) {
+  //     if (secretProperties.enabled) {
+  //       const secret = await client.getSecret(secretProperties.name);
+  //       console.log("secret: ", secret);
+  //     }
+  //   }
+  //   console.log("--page--");
+  // }
+
+  //   // List the secrets we have, all at once
+  // console.log("Listing secrets all at once");
+  // for await (const secretProperties of client.listPropertiesOfSecrets()) {
+  //   if (secretProperties.enabled) {
+  //     const secret = await client.getSecret(secretProperties.name);
+  //     console.log("secret: ", secret);
+  //   }
+  // }
   
 }
 
