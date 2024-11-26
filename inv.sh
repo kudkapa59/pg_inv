6a2c6ccd8de3b9e60a50d1175c25a17f3fa00cac
Kapa Kudaibergenov <kudkapa59@gmail.com>
2024-11-26 13:17:08 +0100

    Update inv.sh

--- inv.sh
+++ inv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #install the database schema
-./pg_inv.sh database -i
+#./pg_inv.sh database -i
 #install the data_loading (creates the cronjobs)
-./pg_inv.sh data_loading -i
+#./pg_inv.sh data_loading -i
 #install the dashboards
 ./pg_inv.sh dashboard -i
 #testing the data_loading
-./pg_inv.sh data_loading -s
+#./pg_inv.sh data_loading -s
