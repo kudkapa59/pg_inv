9ee633ae026dc7d2e880f8dfc3706a5c7077f55a
Kapa Kudaibergenov <kudkapa59@gmail.com>
2024-12-03 14:51:12 +0100

    Update PostgresConnection.ts

--- dashboard/backend/PostgresConnection.ts
+++ dashboard/backend/PostgresConnection.ts
@@ -1,9 +1,15 @@
 import { Pool } from 'pg';
 
 export const pgConn = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: (process.env.PGPORT as unknown as number),
+    // user: process.env.PGUSER,
+    // host: process.env.PGHOST,
+    // database: process.env.PGDATABASE,
+    // password: process.env.PGPASSWORD,
+    // port: (process.env.PGPORT as unknown as number),
+     host: process.env.AZURE_POSTGRESQL_HOST,
+     user: process.env.AZURE_POSTGRESQL_USER,
+     password: process.env.AZURE_POSTGRESQL_PASSWORD,
+     database: process.env.AZURE_POSTGRESQL_DATABASE,
+     port: Number(process.env.AZURE_POSTGRESQL_PORT) ,
+     ssl: process.env.AZURE_POSTGRESQL_SSL
 });
